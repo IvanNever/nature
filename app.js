c0289fc9affe11cd0964754a6049608b02c7b434
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-01-11 17:56:23 +0200

    create some middlewares, env variables and eslint configuration

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //Middlewares
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello from the  middleware');
@@ -21,4 +24,3 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
-
