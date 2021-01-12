0eee39119316d96fae83ef000f1d1b6bb955a320
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-01-12 15:53:31 +0200

    refactoring for MVC

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //Middlewares
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-    console.log('Hello from the  middleware');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('Hello from the  middleware');
+//     next();
+// });
 
 //Routes
 app.use('/api/v1/tours', tourRouter);
