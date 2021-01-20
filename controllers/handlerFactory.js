1eb01c2fcbc8075fad3fd811b74ea2cc97cd15a9
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-01-20 22:06:06 +0200

    add missed protect and auth to all routes

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -4,8 +4,12 @@ const AppError = require('../utils/appError');
 
 exports.getAll = (Model) =>
     catchAsync(async (req, res, next) => {
+        //To allow for nested GET reviews on tour (hack)
+        let filter = {};
+        if (req.params.tourId) filter = { tour: req.params.tourId };
+
         //Exequte query
-        const features = new APIFeatures(Model.find(), req.query)
+        const features = new APIFeatures(Model.find(filter), req.query)
             .filter()
             .sort()
             .limitFields()
