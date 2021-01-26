5b3568f9bb924d4e4488e77a9d19fca7cae03b6f
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-01-26 17:49:01 +0200

    integration stripe payments

---
+++ public/js/stripe.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const bookTour = async (tourId) => {
+    const stripe = Stripe(
+        'pk_test_51IDnXgCzQwQ0j8gOJbf3FRQ46iv8Sb5AXNpASCe2E3Uta7QcCdDVnevbsyNynicg2qusBH5u3VxlgUbipdCujCoQ00MOXWf2bs'
+    );
+    try {
+        // 1) Get checkout session from API
+        const session = await axios(
+            `http://127.0.0.1:3000/api/v1/booking/checkout-session/${tourId}`
+        );
+
+        // 2) Create checkout form + charge credit card
+        await stripe.redirectToCheckout({
+            sessionId: session.data.session.id,
+        });
+    } catch (err) {
+        console.log(err);
+        showAlert('error', err);
+    }
+};
